61013efe88da2cd1b05d5ba2d4261f75298e5fe1
JosephPM39 <josias.jjpm49@hotmail.com>
2022-03-29 18:47:42 -0600

    Implementando estados en burbujas para cuando se haga scroll

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { GlobalStyle } from "./styles/GlobalStyles";
-import { PhotoCard } from "./components/PhotoCard";
 import { Logo } from './components/Logo'
+import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
 
 export const App = () => (
   <>
     <GlobalStyle />
     <Logo />
     <ListOfCategories />
-    <PhotoCard />
+    <ListOfPhotoCards />
   </>
 );
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,45 @@
-import React from 'react'
-import { Category } from '../Category'
-import { List, Item } from './styles'
-import data  from '../../../api/db.json'
+import React, { Fragment, useEffect, useState } from "react";
+import { Category } from "../Category";
+import data from "../../../api/db.json";
+
+import { List, Item } from "./styles";
 
 export const ListOfCategories = () => {
-  return (
-    <List>
-      {
-        data.categories.map(category=><Item key={category.id}><Category {...category} /></Item>)
-      }
+  const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(function () {
+    setCategories(data.categories);
+  }, []);
+
+  useEffect(
+    function () {
+      const onScroll = (e) => {
+        const newShowFixed = window.scrollY > 200;
+        showFixed !== newShowFixed && setShowFixed(newShowFixed);
+      };
+
+      document.addEventListener("scroll", onScroll);
+
+      return () => document.removeEventListener("scroll", onScroll);
+    },
+    [showFixed]
+  );
+
+  const renderList = (fixed) => (
+    <List className={fixed ? "fixed" : ""}>
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
     </List>
-  )
-}
+  );
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
+  );
+};
